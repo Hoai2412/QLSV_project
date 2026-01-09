25349d548bf1c5d92d7f758b270f4ce07a3517c6
Hoai2412 <tranthuhoai24122004@gmail.com>
2026-01-09 21:41:26 +0700

    Initial commit QLSV project

--- qlsv/controllers/adminController.js
+++
@@ -1,7 +0,0 @@
-// controllers/adminController.js
-const path = require('path');
-
-exports.getDashboard = (req, res) => {
-    const filePath = path.join(__dirname, '..', 'views', 'admin', 'admin-dashboard.html');
-    return res.sendFile(filePath);
-};
